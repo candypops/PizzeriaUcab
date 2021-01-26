2d28bca9810566c65e733e63f4b588fbafc48c5b
Miguel Coccaro <mecoccaro.16@est.ucab.edu.ve>
2021-01-26 11:38:12 -0400

    Arreglos a la bd

--- polls/models.py
+++ polls/models.py
@@ -26,12 +26,8 @@ class Ingrediente(models.Model):
     abreviado = models.CharField(max_length=2)
     costo = models.FloatField()
 
-class Pizza(models.Model):
-    nombre = models.CharField(max_length=50)
-    abreviado = models.CharField(max_length=2)
-    tamano_pizza = models.ForeignKey(Tamano, on_delete=models.CASCADE)
-    ingrediente_pizza = models.ForeignKey(Ingrediente, on_delete=models.CASCADE)
 
 class Pedido_Pizza(models.Model):
     pedido_fk = models.ForeignKey(Pedido, on_delete=models.CASCADE)
-    pizza_fk = models.ForeignKey(Pizza, on_delete=models.CASCADE)
+    pizza_fk = models.ForeignKey(Ingrediente, on_delete=models.CASCADE)
+    tamano_pizza = models.ForeignKey(Tamano, on_delete=models.CASCADE)
