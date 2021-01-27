290b4a6d2c0496267e6e024e253fcad601adc4c5
mecoccaro <36824674+mecoccaro@users.noreply.github.com>
2021-01-27 18:08:27 -0400

    Nuevas adiciones con la base de datos (#1)
    * modificaciones en los ajustes
    
    * Agregado al admin combo, tamaño e ingredientes
    
    * Agregados los datos a las tablas tamano e ingredientes

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Ingrediente, Tamano, Combo
 
-# Register your models here.
+admin.site.register(Ingrediente)
+admin.site.register(Tamano)
+admin.site.register(Combo)
+
+
+
--- polls/views.py
+++ polls/views.py
@@ -1,12 +1,5 @@
 from django.shortcuts import render
-
-
 from django.http import HttpResponse
 
-
-
-
 def index(request):
-
-
  return HttpResponse("Hola UCAB, usted está en el indice de polls")
