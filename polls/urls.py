36cbf523224b6b16d5eb6345bd97023b0beabb42
Miguel Coccaro <mecoccaro.16@est.ucab.edu.ve>
2021-02-02 23:20:38 -0400

    Funcionalidades basicas agregadas

--- polls/urls.py
+++ polls/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
  path('', views.index, name='index'),
- path('tam/', views.createPedido, name='tamanos'),
  path('ing/', views.mostrarIngredientes, name='ingredientes'),
- path(r'^factura/$', views.factura, name='factura')
+ path(r'^factura/$', views.factura, name='factura'),
+ path('new/', views.newPizza, name='nueva'),
 ]
