68355a1949f3cffaca52dfeab8fe2d772a4b5a65
Miguel Coccaro <mecoccaro.16@est.ucab.edu.ve>
2021-02-03 19:58:52 -0400

    Pizzería base funcional

---
+++ first_inserts.sql
@@ -0,0 +1,12 @@
+insert into polls_tamano(tamano, costo) values ('Grande',20 );
+insert into polls_tamano(tamano, costo) values ('Mediana',16 );
+insert into polls_tamano(tamano, costo) values ('Personal',10 );
+
+insert into polls_ingrediente(nombre, costo) VALUES ('Jamón', 4);
+insert into polls_ingrediente(nombre, costo) VALUES ('Champiñones', 3.5);
+insert into polls_ingrediente(nombre, costo) VALUES ('Pimentón', 3);
+insert into polls_ingrediente(nombre, costo) VALUES ('Doble Queso', 4);
+insert into polls_ingrediente(nombre, costo) VALUES ('Aceitunas', 5.75);
+insert into polls_ingrediente(nombre, costo) VALUES ('Pepperoni', 3.85);
+insert into polls_ingrediente(nombre, costo) VALUES ('Salchichón', 6.25);
+insert into polls_ingrediente(nombre, costo) VALUES ('Ninguno', 0);
